1e2a5ec8bca21bf157d81360074133705ef4a6e4
vasil <vasilbazhiv12@gmail.com>
2023-06-03 09:34:29 +0300

    new-adapt-section-over

--- src/js/my/main.js
+++ src/js/my/main.js
@@ -1,22 +1,21 @@
-$(function() {
-	$(".services-accordion").accordion({
-		'collapsible': true,
-		'active': 0
-	});
-});
+$(function () {
+	$('.services-accordion').accordion({
+		collapsible: true,
+		active: 0,
+	})
+})
 //items open detail
 const allItemsSer = document.querySelector('.services__items')
 
-
-allItemsSer?.addEventListener('click', (e)=>{
+allItemsSer?.addEventListener('click', e => {
 	const elem = e.target
 	const open = elem.classList.contains('services-item__btn')
 	const close = elem.classList.contains('services-detail__close')
-	if(open){
+	if (open) {
 		const item = elem.parentNode.parentNode
 		item.classList.toggle('services-item-open-det')
 	}
-	if(close){
+	if (close) {
 		const item = elem.parentNode.parentNode.parentNode.parentNode
 		console.log(item)
 		item.classList.toggle('services-item-open-det')
@@ -26,115 +25,137 @@ allItemsSer?.addEventListener('click', (e)=>{
 //map
 const mapImg = document.querySelector('.map-img')
 
-mapImg?.addEventListener('mouseover', (e)=>{
+mapImg?.addEventListener('mouseover', e => {
 	const elem = e.target.dataset.piece
-	const element = document.querySelector('[data-text="'+ elem +'"]');
+	const element = document.querySelector('[data-text="' + elem + '"]')
 	const active = document.querySelector('.active-path-text')
-	if(active){
+	if (active) {
 		active.classList.remove('active-path-text')
 	}
 	element.classList.add('active-path-text')
 })
 
 //slider one
-$(document).ready(function(){
-  var slider = $('.triumphs-slider__items');
+$(document).ready(function () {
+	var slider = $('.triumphs-slider__items')
 
-  slider.slick({
-    // Опції слайдера Slick тут
-    dots: true,
-    prevArrow: $('.triumphs-slider__prev'),
-    nextArrow: $('.triumphs-slider__next'),
-    appendDots: $('.triumphs-slider__sum')
-  });
+	slider.slick({
+		// Опції слайдера Slick тут
+		dots: true,
+		prevArrow: $('.triumphs-slider__prev'),
+		nextArrow: $('.triumphs-slider__next'),
+		appendDots: $('.triumphs-slider__sum'),
+	})
 
-  var slideCount = slider.slick('getSlick').slideCount;
-  var currentSlide = slider.slick('slickCurrentSlide') + 1;
+	var slideCount = slider.slick('getSlick').slideCount
+	var currentSlide = slider.slick('slickCurrentSlide') + 1
 
-  updateSlideCount(slideCount, currentSlide);
+	updateSlideCount(slideCount, currentSlide)
 
-  slider.on('afterChange', function(event, slick, currentSlide){
-    var currentSlideNumber = currentSlide + 1;
-    updateSlideCount(slideCount, currentSlideNumber);
-  });
+	slider.on('afterChange', function (event, slick, currentSlide) {
+		var currentSlideNumber = currentSlide + 1
+		updateSlideCount(slideCount, currentSlideNumber)
+	})
 
-  function updateSlideCount(totalSlides, currentSlideNumber) {
-    if (currentSlideNumber) {
-      $('.triumphs-slider__sum').text(formatSlideNumber(currentSlideNumber) + ' / ' + formatSlideNumber(totalSlides));
-    } else {
-      $('.triumphs-slider__sum').text(formatSlideNumber(totalSlides));
-    }
-  }
+	function updateSlideCount(totalSlides, currentSlideNumber) {
+		if (currentSlideNumber) {
+			$('.triumphs-slider__sum').text(
+				formatSlideNumber(currentSlideNumber) +
+					' / ' +
+					formatSlideNumber(totalSlides)
+			)
+		} else {
+			$('.triumphs-slider__sum').text(formatSlideNumber(totalSlides))
+		}
+	}
 
-  function formatSlideNumber(number) {
-    return number?.toString();
-  }
-});
+	function formatSlideNumber(number) {
+		return number?.toString()
+	}
+})
 
 //slider-blog
 var swiper = new Swiper('.blog-slider__swiper', {
-  slidesPerView: 3,
-  spaceBetween: 41,
-  navigation: {
-    nextEl: '.swiper-blog-next',
-    prevEl: '.swiper-blog-prev',
-  },
-});
+	navigation: {
+		nextEl: '.swiper-blog-next',
+		prevEl: '.swiper-blog-prev',
+	},
+	breakpoints: {
+		// Опции для разных брейкпоинтов
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		670: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		750: {
+			slidesPerView: 2,
+			spaceBetween: 60,
+		},
+		980: {
+			slidesPerView: 2,
+			spaceBetween: 101,
+		},
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 41,
+		},
+	},
+})
 //burg
-function switchBody(newClass, also){
-  const body = document.querySelector('body')
-  if(body.classList.contains('open')){
-    body.classList = ''
-    if(also){
-      body.classList.add(newClass)
-      body.classList.add('open')
-    }
-  }else{
-    if(newClass){
-      body.classList.add(newClass)
-      body.classList.add('open')
-    }
-  }
-  
- 
+function switchBody(newClass, also) {
+	const body = document.querySelector('body')
+	if (body.classList.contains('open')) {
+		body.classList = ''
+		if (also) {
+			body.classList.add(newClass)
+			body.classList.add('open')
+		}
+	} else {
+		if (newClass) {
+			body.classList.add(newClass)
+			body.classList.add('open')
+		}
+	}
 }
-function getElem(e, classCheck){
-  const elem = e.target
-  if(elem.classList.contains(classCheck)){
-    return elem
-  }
+function getElem(e, classCheck) {
+	const elem = e.target
+	if (elem.classList.contains(classCheck)) {
+		return elem
+	}
 }
 const btnBurg = document.querySelector('.icon-menu')
 const menu = document.querySelector('.menu')
 const tellBtn = document.querySelector('.header-tell')
 
-btnBurg?.addEventListener('click', ()=>{
-  switchBody('menu-open')
+btnBurg?.addEventListener('click', () => {
+	switchBody('menu-open')
 })
 
-menu?.addEventListener('click', (e)=>{
-  const elem = getElem(e, 'menu')
-  if(elem){
-    switchBody('menu-open')
-  }
+menu?.addEventListener('click', e => {
+	const elem = getElem(e, 'menu')
+	if (elem) {
+		switchBody('menu-open')
+	}
 })
 
-tellBtn.addEventListener('click', ()=>{
-  switchBody('header-contacts-open', true)
+tellBtn.addEventListener('click', () => {
+	switchBody('header-contacts-open', true)
 })
 
 //lang
 function handleResize() {
-  var screenWidth = $(window).width();
+	var screenWidth = $(window).width()
 
-  if (screenWidth < 590) {
-    var languageElement = $('.language');
-    var menuListElement = $('.menu__list');
+	if (screenWidth < 590) {
+		var languageElement = $('.language')
+		var menuListElement = $('.menu__list')
 
-    languageElement.detach().appendTo(menuListElement);
-  }
+		languageElement.detach().appendTo(menuListElement)
+	}
 }
 
-$(window).on('resize', handleResize);
-$(document).ready(handleResize);
-
+$(window).on('resize', handleResize)
+$(document).ready(handleResize)
